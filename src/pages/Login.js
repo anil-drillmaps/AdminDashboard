61b73211087b9fd31ec7019fb6d8d9140b710e2c
Anil Kumar <anil@drillmaps.com>
2023-05-10 10:41:13 +0530

    add login and signup page with firebase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,7 @@
 import {
-  // Avatar,
-  // Box,
   Button,
   Card,
   Dialog,
-  DialogTitle,
   FormControl,
   FormGroup,
   Grid,
@@ -12,13 +9,7 @@ import {
   InputAdornment,
   InputLabel,
   Link,
-  List,
-  ListItem,
-  // ListItemAvatar,
-  ListItemButton,
-  ListItemText,
   OutlinedInput,
-  // Popper,
   Stack,
   TextField,
   Typography,
@@ -28,79 +19,33 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import PropTypes from 'prop-types';
-function SimpleDialog(props) {
-  const { onClose, selectedValue, open } = props;
-
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
-
-  const handleListItemClick = (value) => {
-    onClose(value);
-  };
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-          <ListItem disableGutters>
-            <ListItemButton
-              
-            
-            >
-           
-              <ListItemText primary="helllo" />
-            </ListItemButton>
-          </ListItem>
-       
-
-        <ListItem disableGutters>
-          <ListItemButton
-            autoFocus
-            onClick={() => handleListItemClick("addAccount")}
-          >
-           
-            <ListItemText primary="Add account" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Dialog>
-  );
-}
-
-SimpleDialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  selectedValue: PropTypes.string.isRequired,
-};
+// import PropTypes from "prop-types";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import Signup from "./Signup";
 
 function Login() {
   const [email, setEmail] = useState("");
-  const [passward, setPassward] = useState("");
-
-  const userName = localStorage.getItem("email")
-    ? localStorage.getItem("email")
-    : "anil@gmail.com";
-  const userPassward = localStorage.getItem("passward")
-    ? localStorage.getItem("passward")
-    : "Anil@123";
+  const [password, setPassword] = useState(""); //Dialog for registration form
+  const [open, setOpen] = React.useState(false); // Using Local storage ; // const userName = localStorage.getItem("email") //   ? localStorage.getItem("email") //   : "anil@gmail.com"; // const userPassward = localStorage.getItem("passward") //   ? localStorage.getItem("passward") //   : "Anil@123"; // const navigate = useNavigate(); // const handleSubmit = (e) => { //   e.preventDefault(); //   if (email === userName && password === userPassward) { //     toast.success("Login Success!", { //       position: toast.POSITION.TOP_CENTER, //     }); //     console.warn("success!"); //     navigate("/homepage"); //   } else { //     toast.error("Invalid Email or Passward!", { //       position: toast.POSITION.TOP_CENTER, //     }); //     navigate("/"); //   } // };
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
+  const signIn = (e) => {
     e.preventDefault();
-    if (email === userName && passward === userPassward) {
-      toast.success("Login Success!", {
-        position: toast.POSITION.TOP_CENTER,
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        toast.success("SignIn Success!", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        navigate("/homepage");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("User Not Fonud! ", {
+          position: toast.POSITION.TOP_CENTER,
+        });
       });
-      console.warn("success!");
-      navigate("/homepage");
-    } else {
-      toast.error("Invalid Email or Passward!", {
-        position: toast.POSITION.TOP_CENTER,
-      });
-      navigate("/");
-    }
-  };
+  }; //show and view passward with icon
   const [showPassword, setShowPassword] = React.useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -108,22 +53,20 @@ function Login() {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-  //popobar
-  const [open, setOpen] = React.useState(false);
-  const [selectedValue, setSelectedValue] = React.useState();
+
+  const handleCloseModel = () => {
+    setOpen(false);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = (value) => {
-    setOpen(false);
-    setSelectedValue(value);
-  };
-
   return (
     <>
+     
       <div>
+       
         <Grid
           xs={12}
           container
@@ -132,10 +75,13 @@ function Login() {
           justifyContent="space-between"
           alignItems="center"
         >
+         
           <Grid item sm={6} xs={12} lg={6} justifyContent="space-around">
+        
             <Card
               sx={{ minHeight: 880, maxWidth: 475, m: 2, borderRadius: "13px" }}
             >
+            
               <Stack
                 elevation={5}
                 direction={"column"}
@@ -143,25 +89,33 @@ function Login() {
                 alignItems={"flex-start"}
                 mt={2}
               >
+               
                 <Link href="#">
+                  
                   <img src="./logo.svg" alt="logo" height={30} width={150} />
+                 
                 </Link>
+            
                 <Typography
                   sx={{ fontSize: 29, fontWeight: 600, margin: 3, mt: 0.5 }}
                 >
-                  Hi, Welcome to
-                  <br />
-                  Subscription App
+                Hi, Welcome to  <br />
+                  Subscription App 
                 </Typography>
+               
                 <img
                   src="./illustration_login.png"
                   alt="illustration_login"
                   width={"100%"}
                 />
+              
               </Stack>
+           
             </Card>
+         
           </Grid>
           <Grid item sm={6} xs={12} lg={6} justifyContent={"space-around"}>
+           
             <Stack
               direction={"column"}
               justifyContent={"flex-start"}
@@ -169,103 +123,126 @@ function Login() {
               mt={2}
               sx={{ minHeight: 580, maxWidth: 395, m: 3 }}
             >
+             
               <Typography variant="h5" fontWeight={600}>
-                Sign in
+               Sign in 
               </Typography>
+             
               <Typography
                 variant="span"
                 color={"gray"}
                 fontSize={13}
                 marginTop={1}
               >
-                Enter your details below.
+              Enter your details below. 
               </Typography>
-              <FormGroup>
-                <FormControl>
-                  <TextField
-                    color="warning"
-                    value={email}
-                    variant="outlined"
-                    size="medium"
-                    className="form-control"
-                    label="Email Address"
-                    onChange={(e) => setEmail(e.target.value)}
-                    fullWidth
-                    sx={{ mt: 4 }}
-                  />
-                  <br />
-                  <FormControl
-                    variant="outlined"
-                    color="warning"
-                    fullWidth
-                    size="small"
-                  >
-                    <InputLabel htmlFor="outlined-adornment-password">
-                      Password
-                    </InputLabel>
-                    <OutlinedInput
-                      value={passward}
-                      type={showPassword ? "text" : "password"}
+             
+              <form //  onSubmit={signIn}
+              >
+                <FormGroup>
+                 
+                  <FormControl>
+                   
+                    <TextField
+                      color="warning"
+                      value={email}
+                      variant="outlined"
                       size="medium"
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                          >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                      label="Password"
-                      onChange={(e) => setPassward(e.target.value)}
+                      className="form-control"
+                      label="Email Address"
+                      onChange={(e) => setEmail(e.target.value)}
+                      fullWidth
+                      sx={{ mt: 4 }}
                     />
+                    <br />
+                    <FormControl
+                      variant="outlined"
+                      color="warning"
+                      fullWidth
+                      size="small"
+                    >
+                      <InputLabel htmlFor="outlined-adornment-password">
+                        Password
+                      </InputLabel>
+                      <OutlinedInput
+                        value={password}
+                        type={showPassword ? "text" : "password"}
+                        size="medium"
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}
+                              edge="end"
+                            >
+                              {showPassword ? (
+                                <VisibilityOff />
+                              ) : (
+                                <Visibility />
+                              )}
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                        label="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </FormControl>
+                 
+                    <Link
+                      href="#"
+                      mt={1}
+                      fontSize={13}
+                      sx={{ color: "orangered" }}
+                    >
+                      Forget Password 
+                    </Link>
+                    
+                    <Button
+                      variant="contained"
+                      fullWidth
+                      color="warning"
+                      sx={{ mt: 1.5 }}
+                      type="submit"
+                      onClick={signIn}
+                    >
+                      Sign in 
+                    </Button>
+                  
+                    <Link mt={1} fontSize={13} sx={{ color: "green" }}>
+                      Don’t have an account? 
+                      
+                    </Link>
+                    
+                    <Button
+                      variant="contained"
+                      fullWidth
+                      color="success"
+                      sx={{ mt: 1.5 }} // aria-describedby={id}
+                      type="button" // onClick={handleClickOpen}
+                      onClick={handleClickOpen}
+                    >
+                       Get Started 
+                    </Button>
+                    
                   </FormControl>
-                  <Link
-                    href="#"
-                    mt={1}
-                    fontSize={13}
-                    sx={{ color: "orangered" }}
-                  >
-                    Forget Password
-                  </Link>
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    color="warning"
-                    sx={{ mt: 1.5 }}
-                    onClick={handleSubmit}
-                  >
-                    Sign in
-                  </Button>
-                  <Link mt={1} fontSize={13} sx={{ color: "green" }}>
-                    Don’t have an account?
-                  </Link>
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    color="success"
-                    sx={{ mt: 1.5 }}
-                    // aria-describedby={id}
-                    type="button"
-                    // onClick={handleClickOpen}
-                    onClick={handleClickOpen}
-                  >
-                    Get Started
-                  </Button>
-                  <SimpleDialog
-                    selectedValue={selectedValue}
-                    open={open}
-                    onClose={handleClose}
-                  />
-                </FormControl>
-              </FormGroup>
+               
+                </FormGroup>
+               
+              </form>
+             
             </Stack>
+           
           </Grid>
+          
         </Grid>
+        
+        <Dialog onClose={handleCloseModel} open={open}>
+         <Signup onClose={setOpen} />
+        </Dialog>
+        
       </div>
+  
     </>
   );
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,151 @@
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+function Signup({ onClose }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        toast.success("Registration success!", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        navigate("/");
+        onClose(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Please Enter Correct Details!", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        navigate("/");
+      });
+  };
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+   
+      <Grid container justifyContent={"space-around"}>
+       
+        <Stack
+          direction={"column"}
+          justifyContent={"flex-start"}
+          alignItems={"flex-center"}
+          mt={2}
+          sx={{ minHeight: 280, minWidth: 295, m: 3 }}
+        >
+         
+          <Typography variant="h5" fontWeight={600}>
+           Sign in
+          </Typography>
+         
+          <Typography variant="span" color={"gray"} fontSize={13} marginTop={1}>
+            Enter your details below.
+          </Typography>
+    
+          <form //  onSubmit={signUp}
+          >
+         
+            <FormGroup>
+           
+              <FormControl>
+                
+                <TextField
+                  color="warning"
+                  value={email}
+                  variant="outlined"
+                  size="medium"
+                  className="form-control"
+                  label="Email Address"
+                  onChange={(e) => setEmail(e.target.value)}
+                  fullWidth
+                  sx={{ mt: 4 }}
+                />
+               <br />
+                <FormControl
+                  variant="outlined"
+                  color="warning"
+                  fullWidth
+                  size="small"
+                >
+                  
+                  <InputLabel htmlFor="outlined-adornment-password">
+                   Password
+                  </InputLabel>
+                
+                  <OutlinedInput
+                    value={password}
+                    type={showPassword ? "text" : "password"}
+                    size="medium"
+                    endAdornment={
+                      <InputAdornment position="end">
+                       
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                         
+                          {showPassword ? <VisibilityOff /> : <Visibility />}   
+                         
+                        </IconButton>
+                       
+                      </InputAdornment>
+                    }
+                    label="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+             
+                </FormControl>
+               
+                <Button
+                  variant="contained"
+                  fullWidth
+                  color="success"
+                  sx={{ mt: 1.5 }} // aria-describedby={id}
+                  type="submit" // onClick={handleClickOpen}
+                  onClick={signUp}
+                >
+                  Create User
+                </Button>
+              
+              </FormControl>
+    
+            </FormGroup>
+         
+          </form>
+         
+        </Stack>
+       
+      </Grid>
+     
+    </>
+  );
+}
+export default Signup
